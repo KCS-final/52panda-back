55af7cb8a145bd772c06c17638ab3c09e3b6578a
Sang Min Park <minisstar@naver.com>
2024-04-23 16:57:52 +0900

    feat: 유저 엔터티 수정

--- src/main/java/com/kcs3/panda/domain/user/entity/User.java
+++ src/main/java/com/kcs3/panda/domain/user/entity/User.java
@@ -1,15 +1,12 @@
 package com.kcs3.panda.domain.user.entity;
 
+import com.kcs3.panda.domain.model.BaseEntity;
 import jakarta.persistence.*;
 import lombok.Data;  // Lombok의 Data 어노테이션을 사용
 
 @Data  // Lombok을 통해 기본 메소드 자동 생성
 @Entity  // JPA 엔터티임을 나타냅니다.
-public class User {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)  // AUTO-INCREMENT
-    private Long userId;  // 회원 ID, LONG 타입
+public class User extends BaseEntity {
 
     @Column(nullable = false, length = 255)  // NOT NULL, VARCHAR(255)
     private String userNickname;  // 닉네임
