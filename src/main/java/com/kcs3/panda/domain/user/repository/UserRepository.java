994520869396dd76cc688cddf692bd0b493eb140
Sang Min Park <minisstar@naver.com>
2024-04-29 13:42:30 +0900

    feat: 유저레포지토리 구현

--- src/main/java/com/kcs3/panda/domain/user/repository/UserRepository.java
+++ src/main/java/com/kcs3/panda/domain/user/repository/UserRepository.java
@@ -6,9 +6,13 @@ import com.kcs3.panda.domain.user.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    @Transactional(readOnly = true)
     User findByUserNickname(String nickname);  // 닉네임으로 사용자 찾기
 
-    @Transactional(readOnly = true)  // 읽기 전용 트랜잭션
+    @Transactional(readOnly = true)
     User findByUserEmail(String email);  // 이메일로 사용자 찾기
+
+    @Transactional(readOnly = true)
+    User findByUserId(Long userId);  // 유저 ID로 사용자 찾기
 }
 
